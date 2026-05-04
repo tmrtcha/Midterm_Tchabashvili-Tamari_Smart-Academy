74fccb09c53b2978be9b85908329e9e73281757a
tmrtcha <tchabashvili.tamari@gmail.com>
2026-05-05 02:02:24 +0400

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,17 +6,14 @@ import styles from "./navbar.module.css";
 export default function Navbar() {
   return (
     <nav className={styles.nav}>
-      {/* LOGO */}
       <div className={styles.logo}>amazon</div>
 
-      {/* MENU */}
       <div className={styles.menu}>
         <Link href="/products">Products</Link>
         <Link href="/profile">Profile</Link>
         <Link href="/cart">Cart</Link>
       </div>
 
-      {/* SEARCH */}
       <div className={styles.searchBox}>
         <input type="text" placeholder="Search products..." />
         <button>🔍</button>
