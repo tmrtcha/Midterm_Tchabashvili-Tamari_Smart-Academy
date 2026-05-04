aa810aff88a37c5fdf9d492de1d242c3012d364b
tmrtcha <tchabashvili.tamari@gmail.com>
2026-05-04 23:33:27 +0400

    Font-Poppins

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,12 @@
 import "./globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+});
 
 export const metadata = {
   title: "My Store",
@@ -10,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={poppins.className}>
         <Navbar />
         {children}
         <Footer />
